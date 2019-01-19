e1ac2b88192c91bf4ec8998c6c960aabef0c7a5e
rfu <rfu@snapchat.com>
2019-01-18 23:20:56 -0800

    add frame header

---
+++ src/common/frame.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import FrameHeader from "./frame-header";
+
+class Frame extends React.Component {
+  render() {
+    return (
+      <div>
+        <FrameHeader />
+      </div>
+    );
+  }
+}
+
+export default Frame;
